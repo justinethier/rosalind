a5ca88e5d12027e759d76dcbf7329da76755d012
Justin Ethier <justin.ethier@gmail.com>
2013-01-15 22:50:49 -0500

    Added code to traverse the fasta data.

--- src/grph.c
+++ src/grph.c
@@ -11,16 +11,31 @@
 #include "lib/fasta_lib.h"
 #include "../../linked-list/linked-list.h"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     char sample[] = "data/grph.fas", *file = sample;
     if (argc > 1) 
         file = argv[1];
-    struct node *data = fasta_read_data(file);
+    struct node *fdata = fasta_read_data(file);
+    struct node *m, *n;
 
-    // TODO: process file data
+    printf("processing file data\n");
+    m = fdata;
+    while (m){
+        struct fasta *mf = list_value(m);
+        n = fdata;
+        do {
+            struct fasta *nf = list_value(n);
+            if (mf && nf && strcmp(mf->label, nf->label) == 0){
+                printf("%s", nf->label);
+            }
+            n = list_rest(n);
+        } while (n);
+        m = list_rest(m);
+    }
 
     // Clean up
-    list_destroy(data, fasta_dealloc);
+    list_destroy(fdata, fasta_dealloc);
     return 0;
 }
 
